11ca5625986657369bd97302cfd53315ef5922ff
Naravan266 <akarzumanian@gmail.com>
2025-11-03 14:14:44 -0300

    Initial Commit

---
+++ CityBase/CityBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityBaseApp.swift
+//  CityBase
+//
+//  Created by Naira on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CityBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityBaseTests/CityBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityBaseTests.swift
+//  CityBaseTests
+//
+//  Created by Naira on 03.11.2025.
+//
+
+import Testing
+@testable import CityBase
+
+struct CityBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
